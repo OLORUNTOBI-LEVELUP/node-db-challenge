cfd069851239514c7661c6455a4f9b7257d23a56
oloruntobi <awoderuoloruntobi@gmail.com>
2019-07-19 08:51:57 +0100

    Completed projects table

--- data/migrations/20190719084053_projectActionsTable.js
+++ data/migrations/20190719084053_projectActionsTable.js
@@ -1,8 +1,12 @@
-
 exports.up = function(knex) {
-  
+  return knex.schema.createTable("projects", tbl => {
+    tbl.increments();
+    tbl.text("name", 128).notNullable();
+    tbl.text("description", 128).notNullable();
+    tbl.boolean("completed").defaultTo(false);
+  });
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTableIfExists("projects").dropTableIfExists("schemes");
 };
